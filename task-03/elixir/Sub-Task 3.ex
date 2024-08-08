7888863a3110192eff6547d38e48c0f001fbdc26
MrKanju <sreejiths252004@gmail.com>
2024-08-09 00:17:50 +0530

    elixir

---
+++ task-03/elixir/Sub-Task 3.ex
@@ -0,0 +1,26 @@
+IO.puts("Enter the number of rows:")
+rows = IO.gets("") |> String.trim() |> String.to_integer()
+for i <- 0..(rows - 2) do
+  for _ <- 0..(rows - i - 1) do
+    IO.write(" ")
+  end
+  for _ <- 0..i do
+    IO.write("* ")
+  end
+  IO.puts("")
+end
+
+# Print the lower part of the diamond
+for i <- 0..(rows - 1) do
+  # Print leading spaces
+  for _ <- 0..i do
+    IO.write(" ")
+  end
+
+  # Print asterisks
+  for _ <- 0..(rows - i - 1) do
+    IO.write("* ")
+  end
+
+  IO.puts("")
+end
---
+++ task-03/ruby/Sub-task 2.rb
@@ -0,0 +1,5 @@
+filec = File.read("input.txt")
+File.write("output.txt", filec)
+fileo = File.read("output.txt")
+puts fileo
+
